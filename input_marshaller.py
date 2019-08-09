b835f0b9de822dfacfb4ee357c73081f047430a2
Thalia Nero <chrisvn00@gmail.com>
2019-08-09 14:40:55 -0400

    Input reading

---
+++ input_marshaller.py
@@ -0,0 +1,10 @@
+
+def marshall(infile):
+    """Reads the clue data from the given file and outputs a list of (clue, solution)."""
+    ls = []
+    with open(infile, 'r') as data:
+        for line in data:
+            clue = line.split(':')
+            if len(clue) == 2:
+                ls.append((clue[0].strip(), clue[1].strip()))
+    return ls
